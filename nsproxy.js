6bd298ba3674ba082293b5179467a48e03d3051e
David Walker <d0sboots@gmail.com>
2023-01-03 19:07:38 -0800

    Add nsproxy

---
+++ nsproxy.js
@@ -0,0 +1,23 @@
+/** @param {NS} ns */
+export default NSProxy;
+
+function NSProxy(ns) {
+  const memoed = {};
+  const handler = {
+    get(target, prop, receiver) {
+      const ours = memoed[prop];
+      if (ours) return ours;
+      const field = Reflect.get(target, prop, receiver);
+      if (!field) return field;
+      if (typeof field === "object") {
+        return (memoed[prop] = NSProxy(field));
+      }
+      if (typeof field === "function") {
+        return (memoed[prop] = field.bind(ns));
+      }
+      return (memoed[prop] = field);
+    },
+  };
+
+  return new Proxy(ns, handler);
+}
