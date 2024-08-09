9d5e922bbdc65111bc5df68ef113f386a66df90e
David Walker <d0sboots@gmail.com>
2024-08-09 04:49:15 -0700

    Initial calculateBatchThreads impl, with working tests

--- test/formulas_test.mjs
+++ test/formulas_test.mjs
@@ -29,4 +29,64 @@ describe('Formulas.Hacking', function () {
       expect(hacking.growAmount(server, person, threads2)).is.within(1e9 - 1e-4, 1e9 + 1e-4);
     });
   });
+  describe('#calculateBatchThreads()', function () {
+    it('hgw, fixed g', function () {
+      const server = formulas.mockServer();
+      Object.assign(server, {
+        moneyMax: 2e9,
+        moneyAvailable: 1e4,
+        serverGrowth: 320,
+        baseDifficulty: 30,
+        hackDifficulty: 10,
+        minDifficulty: 10,
+        requiredHackingSkill: 20,
+      });
+      const person = formulas.mockPerson();
+      person.skills.hacking = 50;
+      const opts = {
+        batchType: "hgw",
+        server,
+        person,
+        threadConstant: 50,
+        relError: 1e-10,
+      };
+      const h_threads = extra.calculateBatchThreads(opts);
+      const pmh = hacking.hackPercent(server, person);
+      server.moneyAvailable = server.moneyMax - server.moneyMax * pmh * h_threads;
+      server.hackDifficulty += 0.002 * h_threads;
+      expect(hacking.growAmount(server, person, 49.99999)).is.below(server.moneyMax);
+      expect(hacking.growAmount(server, person, 50)).is.within(server.moneyMax - 1e-4, server.moneyMax);
+    });
+
+    it('hgw, fixed w', function () {
+      const server = formulas.mockServer();
+      Object.assign(server, {
+        moneyMax: 2e9,
+        moneyAvailable: 1e4,
+        serverGrowth: 320,
+        baseDifficulty: 30,
+        hackDifficulty: 10,
+        minDifficulty: 10,
+        requiredHackingSkill: 20,
+      });
+      const person = formulas.mockPerson();
+      person.skills.hacking = 50;
+      const opts = {
+        batchType: "hgw",
+        server,
+        person,
+        threadConstant: 100,
+        threadMultiplier: -0.5,
+        relError: 1e-10,
+        fortify: 0.2,
+      };
+      const h_threads = extra.calculateBatchThreads(opts);
+      const g_threads = 100 - 0.5 * h_threads;
+      const pmh = hacking.hackPercent(server, person);
+      server.moneyAvailable = server.moneyMax - server.moneyMax * pmh * h_threads;
+      server.hackDifficulty += 0.002 * h_threads - opts.fortify;
+      expect(hacking.growAmount(server, person, g_threads - 1e-5)).is.below(server.moneyMax);
+      expect(hacking.growAmount(server, person, g_threads)).is.within(server.moneyMax - 1e-4, server.moneyMax);
+    });
+  });
 });
